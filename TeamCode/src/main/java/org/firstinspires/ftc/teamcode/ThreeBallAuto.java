24140464a8613ee17ffe308adad4b55f1444df56
dhilan <dhilanadesai@gmail.com>
2025-11-15 11:37:44 -0600

    Some Camera test Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ThreeBallAuto.java
@@ -0,0 +1,165 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.acmerobotics.roadrunner.Pose2d;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Autonomous(name="ThreeBallAuto", group="Linear OpMode")
+public class ThreeBallAuto extends LinearOpMode {
+    private ElapsedTime pidTimer = new ElapsedTime();
+    double TURN_P = 0.06;
+    double TURN_D = 0.002;
+    final double TURN_GAIN = 0.02;
+    final double MAX_AUTO_TURN = 0.4;
+    //region HARDWARE DECLARATIONS
+// Drive Motors
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor backLeft = null;
+    private DcMotor backRight = null;
+
+    // Mechanism Motors
+    private DcMotorEx fly1 = null;
+    private DcMotorEx fly2 = null;
+    private DcMotor intake = null;
+
+    // Servos
+    private Servo vertTrans;  // Vertical actuator
+    private CRServo spin = null;    // spino
+    private Servo hood;
+    private final double[] HOOD_POSITIONS = {0.2, 0.4, 0.6, 0.8, 1};//may have to change
+    //SENSOR
+    private AnalogInput spinEncoder;
+
+    //endregion
+
+    //region CAROUSEL SYSTEM
+    // Carousel PIDF Constants
+    private double pidKp = 0.0057;
+    private double pidKi = 0.00166;
+    private double pidKd = 0.00002;
+    private double pidKf = 0.0;
+
+    // Carousel PID State
+    private double integral = 0.0;
+    private double lastError = 0.0;
+    private double integralLimit = 500.0;
+    private double pidLastTimeMs = 0.0;
+
+    // Carousel Control Parameters
+    private final double positionToleranceDeg = 2.0;
+    private final double outputDeadband = 0.03;
+
+    // Carousel Positions (6 presets, every 60 degrees)
+    // 57, 177, and 297 face the intake; others face the transfer
+    private final double[] CAROUSEL_POSITIONS = {57.0, 117.0, 177.0, 237.0, 297.0, 357.0};
+    private int carouselIndex = 0;
+    private int prevCarxouselIndex = 0;
+
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        //region OPERATIONAL VARIABLES
+        // Mechanism States
+        boolean tranOn = false;
+        boolean intakeOn = false;
+        double intakePower = 0;
+        boolean flyOn = false;
+
+        //Tuning Variables
+
+        double lastPAdjustTime = 0;
+        double lastIAdjustTime = 0;
+        double lastDAdjustTime = 0;
+        double lastFAdjustTime = 0;
+
+        // Drive Variables
+        double drive = 0;
+        double strafe = 0;
+        double turn = 0;
+
+        // Flywheel Control
+        double flySpeed = 1160;
+
+        double lastTime = 0;
+
+        //Transfer
+        double vertTranAngle = 0;
+        double transMin = 0.05;//when transfers up
+        double transMid = 0.25;//when its under intake
+        double transMax = 0.45;//shoot
+
+        //endregion
+
+        //region HARDWARE INITIALIZATION
+        // Initialize Drive Motors
+        frontLeft = hardwareMap.get(DcMotor.class, "fl");
+        frontRight = hardwareMap.get(DcMotor.class, "fr");
+        backLeft = hardwareMap.get(DcMotor.class, "bl");
+        backRight = hardwareMap.get(DcMotor.class, "br");
+        fly1 = hardwareMap.get(DcMotorEx.class, "fly1");
+        fly2 = hardwareMap.get(DcMotorEx.class, "fly2");
+        intake = hardwareMap.get(DcMotor.class, "in");
+        spin = hardwareMap.get(CRServo.class, "spin");
+        hood = hardwareMap.get(Servo.class, "hood");
+        vertTrans = hardwareMap.get(Servo.class, "vtrans");
+        spinEncoder = hardwareMap.get(AnalogInput.class, "espin");
+
+
+        // DIRECTIONS
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        fly1.setDirection(DcMotor.Direction.REVERSE);
+        fly2.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+
+        spin.setDirection(CRServo.Direction.FORWARD);
+        hood.setDirection(Servo.Direction.FORWARD);
+
+        //MODES
+        fly1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fly2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //endregion
+
+        //INIT TELEMETRY
+        telemetry.addData("Camera preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch START to start OpMode");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        vertTrans.setPosition(transMin);
+    }
+
+}
